c27c54b7b6f3c3142a226855501e75e389611a21
Alan Cruse <alancruse86@gmail.com>
2016-11-01 23:58:29 +0100

    Completed solution for 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,19 +1,13 @@
 class Book
-
   attr_reader :title
 
   def title=(new_book_title)
     corrected_book_title = new_book_title.split(' ')
-
     corrected_book_title.each do |c|
-      unless %w(and of the a an in).include?(c) && c != c[0]
+      unless %w(and of the a an in).include?(c) && c != corrected_book_title[0]
         c.capitalize!
       end
-
     end
-
     @title = corrected_book_title.join(' ')
-
   end
-
 end
