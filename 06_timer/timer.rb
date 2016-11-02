3128c98aeb526b7eea6f5060026e4393e56ecc41
Alan Cruse <alancruse86@gmail.com>
2016-11-02 11:13:44 +0100

    added padded method to timer.rb

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,27 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def padded(n)
+    if n < 10
+      '0' + n.to_s
+    else
+      n.to_s
+    end
+  end
+
+  def time_string
+    if @seconds == 0
+      "00:00:00"
+    else
+      #hour = (@seconds/3600)
+      #remainder = @seconds%3600
+      timer_display = hour.padded(@seconds/3600) + ':'
+
+    end
+
+  end
 end
